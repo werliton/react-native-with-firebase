374f97eea4c63665de8c19dfcd1af569492660bb
werliton <werlitoncarlos@gmail.com>
2017-12-17 10:16:42 +0000

    Criando e salvando usuário no firebase usando o metodo auth()

--- App.js
+++ App.js
@@ -10,11 +10,7 @@ import {
 
 export default class App extends Component<{}> {
 
-    constructor(props){
-        super(props);
-        this.state = {pontuacao: 0};
-    }
-
+  
   componentWillMount(){
        // Initialize Firebase
       var config = {
@@ -27,46 +23,25 @@ export default class App extends Component<{}> {
       };
      firebase.initializeApp(config);
   }
+
+  cadastrarUsuario(){
+    let usuario = firebase.auth();
+    let email = 'werliton.carlos@gmail.com';
+    let senha = 'leto123';
+
+    usuario.createUserWithEmailAndPassword(email, senha);
+  }
  
-  salvarDados(){
-    let pontuacao = firebase.database().ref("pontuacao");
-    pontuacao.child('002').child('ponto').set('150')
-    .then(()=>{
-        Alert.alert('pontuacao incluida com sucesso.');
-      });;
-  }
-
-  listarDados(){
-    let pontuacao = firebase.database().ref('pontuacao');
-    pontuacao.on('value', (snapshot)=>{
-      let ponto = snapshot.value;
-      this.setState({
-        pontuacao: ponto
-      })
-      
-    });
-  }
-
   render() {
     return (
       <View style={{flexDirection:'column'}}>
+       
         <View style={{marginTop:20}}>
-        <Button 
-          onPress={()=>this.salvarDados()}
-          title='Salvar dados'
-          color='#841584'
-        />
-
-        </View>
-
-
-        <View style={{marginTop:20}}>
-          <Button title='Listar dados' 
-          onPress={this.listarDados}
+          <Button title='Cadastrar usuario' 
+          onPress={this.cadastrarUsuario}
           color='#841584'
         />
         </View>
-        <Text style={styles.txtPontos}>Ola mundo aqui estou no linux - {this.state.pontuacao}</Text>
       </View>
     );
   }
